4e5327a9d886ed302759cdc42bf540225a086909
Praveen Polati <praveen_k_polati@optum.com>
2020-01-29 18:49:50 +0530

    Add project files.

---
+++ WebAPICall/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAPICall
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:58144/api/Demo/");
+                //HTTP GET
+                var responseTask = client.GetAsync("");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsStringAsync();
+
+                    var students = readTask.Result;
+                    Label1.Text = students;
+                }
+            }
+        }
+    }
+}
